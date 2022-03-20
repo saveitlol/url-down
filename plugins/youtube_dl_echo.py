29b3f9dc76112164eaa96314c21065cd52028401
Ahmad zaky <53550696+saveitlol@users.noreply.github.com>
2022-03-20 11:11:52 +0700

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -53,7 +53,7 @@ async def echo(bot, update):
             )
             return
         except Exception:
-            await update.reply_text("Something Wrong. Contact @xgorn")
+            await update.reply_text("Something Wrong. Contact @morninginsky")
             return
     logger.info(update.from_user)
     youtube_dl_username = None
